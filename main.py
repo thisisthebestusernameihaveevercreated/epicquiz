25a8f50fe895d99fc86cb72d2a08e46db6b6f7c0
thisisthebestusernameihaveevercreated <19164@my.feildinghigh.school.nz>
2023-03-14 14:23:27 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+
+if __name__ == '__main__':
+    window = tkinter.Tk()
+
+    window.title("quiz")
+    window.geometry("600x600")
+    window.resizable(False, False)
+
+    window.mainloop()
